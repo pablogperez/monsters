09f80576a0d391568b3bf2e7e1a7f433b48e3018
pablogperez <pablyto_18@hotmail.com>
2020-03-17 21:03:05 -0300

    Updates

---
+++ precache-manifest.2acec91b012ad7aae4ab6bc2bd3aac58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a69e38fdefa426fb3b8de47a6d3c136",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "064d6eb1af71d906d4ac",
+    "url": "/monsters/static/css/main.81e7c2c2.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064d6eb1af71d906d4ac",
+    "url": "/monsters/static/js/main.445d8d93.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
